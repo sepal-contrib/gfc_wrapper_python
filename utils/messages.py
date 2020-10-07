6355800401142660a68d03de2e281bf02fd3bbdf
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-10-07 17:15:20 +0000

    describe export process Fix #12

--- utils/messages.py
+++ utils/messages.py
@@ -27,6 +27,14 @@ EXPORT_MESSAGE = 'Click on "Export" to export the data'
 NO_THRESHOLD = 'No threshold have been selected'
 NO_AOI = 'No aoi have been provided, select one in step 1'
 NO_VIZ= 'Befor launching the export, it is strongly advised to vizualize data'
+GFC_TXT = """
+Considering the Aoi selected in step 1 and the threshold selected in step 2 the module will generate a combination of the GFC layers to produce a forest change map, for a given canopy cover threshold. It will live in a ~/gfc_wrapper_results folder of your sepal environment.  
+2 results will be produced:  
+  
+- the map of the forest change mask using the color tab presented in the interactive maps (.tif)
+- the legend of the raster (.pdf)
+- the distribution of each defined zone (.csv)
+"""
 
 
 #########################################
