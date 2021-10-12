9a530defa069892602bf1c40247a7bf97d6f48db
12rambau <pierrick.rambaud49@gmail.com>
2021-10-12 12:14:20 +0000

    build: pre commit hooks

--- component/scripts/compute_areas.py
+++ component/scripts/compute_areas.py
@@ -1,6 +1,9 @@
-import os 
-if 'GDAL_DATA' in list(os.environ.keys()): del os.environ['GDAL_DATA']
-if 'PROJ_LIB' in list(os.environ.keys()): del os.environ['PROJ_LIB']
+import os
+
+if "GDAL_DATA" in list(os.environ.keys()):
+    del os.environ["GDAL_DATA"]
+if "PROJ_LIB" in list(os.environ.keys()):
+    del os.environ["PROJ_LIB"]
 
 from distutils.version import LooseVersion
 
@@ -21,34 +24,34 @@ from .utils import pixel_count, to_hectar
 
 ee.Initialize()
 
+
 def create_hist(result_dir, map_raster, alert):
-    
-    if not map_raster.is_file(): 
-        raise Exception('No gfc map')
-    
+
+    if not map_raster.is_file():
+        raise Exception("No gfc map")
+
     # project raster in world mollweide
     with rio.open(map_raster) as src:
-        
-        proj_crs = CRS.from_string('ESRI:54009')
+
+        proj_crs = CRS.from_string("ESRI:54009")
         if LooseVersion(rio.__gdal_version__) < LooseVersion("3.0.0"):
             rio_crs = RioCRS.from_wkt(proj_crs.to_wkt(WktVersion.WKT1_GDAL))
         else:
             rio_crs = RioCRS.from_wkt(proj_crs.to_wkt())
-        
-        transform, width, height = calculate_default_transform(src.crs, rio_crs, src.width, src.height, *src.bounds)
-    
+
+        transform, width, height = calculate_default_transform(
+            src.crs, rio_crs, src.width, src.height, *src.bounds
+        )
+
         kwargs = src.meta.copy()
-    
-        kwargs.update({
-            'crs': rio_crs,
-            'transform': transform,
-            'width': width,
-            'height': height
-        })
-    
-        map_raster_proj = result_dir/f'{map_raster.stem}_proj.tif'
-        
-        with rio.open(map_raster_proj, 'w', **kwargs) as dst:
+
+        kwargs.update(
+            {"crs": rio_crs, "transform": transform, "width": width, "height": height}
+        )
+
+        map_raster_proj = result_dir / f"{map_raster.stem}_proj.tif"
+
+        with rio.open(map_raster_proj, "w", **kwargs) as dst:
             for i in range(1, src.count + 1):
                 reproject(
                     source=rio.band(src, i),
@@ -57,134 +60,143 @@ def create_hist(result_dir, map_raster, alert):
                     src_crs=src.crs,
                     dst_transform=transform,
                     dst_crs=rio_crs,
-                    resampling=Resampling.nearest
+                    resampling=Resampling.nearest,
                 )
-                
+
             resx, resy = dst.res
-    
+
     # realize a primary hist
     hist = pixel_count(map_raster_proj)
-    
+
     # convert to hectars
-    hist['area'] = to_hectar(hist['pixels'], abs(resx), abs(resy))
-    
+    hist["area"] = to_hectar(hist["pixels"], abs(resx), abs(resy))
+
     return hist
 
+
 def plot_loss(df, aoi_name):
-    
-    d_hist = df[(df['code'] > 0) & (df['code'] < 30)]
+
+    d_hist = df[(df["code"] > 0) & (df["code"] < 30)]
 
     x_sc = LinearScale()
-    y_sc = LinearScale()  
-    
-    ax_x = Axis(label = 'year', scale = x_sc)
-    ax_y = Axis(label = 'tree cover loss surface (ha)', scale = y_sc, orientation = 'vertical') 
-    bar = Bars(x = [i + 2000 for i in d_hist['code']], y = d_hist['area'], scales = {'x': x_sc, 'y': y_sc})
-    title = f'Distribution of tree cover loss per year in {aoi_name}'
-    fig = Figure(
-        title     = title,
-        marks     = [bar], 
-        axes      = [ax_x, ax_y], 
-        padding_x = 0.025, 
-        padding_y = 0.025
+    y_sc = LinearScale()
+
+    ax_x = Axis(label="year", scale=x_sc)
+    ax_y = Axis(
+        label="tree cover loss surface (ha)", scale=y_sc, orientation="vertical"
     )
-    
+    bar = Bars(
+        x=[i + 2000 for i in d_hist["code"]],
+        y=d_hist["area"],
+        scales={"x": x_sc, "y": y_sc},
+    )
+    title = f"Distribution of tree cover loss per year in {aoi_name}"
+    fig = Figure(
+        title=title, marks=[bar], axes=[ax_x, ax_y], padding_x=0.025, padding_y=0.025
+    )
+
     return fig
 
+
 def area_table(df):
-    #construct the total loss line
-    df_loss = df[(df['code'] > 0 ) & (df['code'] < 30)] 
+    # construct the total loss line
+    df_loss = df[(df["code"] > 0) & (df["code"] < 30)]
     df_loss = df_loss.sum()
-    df_loss['code'] = 60
-    df_loss['class'] = 'loss'
+    df_loss["code"] = 60
+    df_loss["class"] = "loss"
     df_masked = df.append(df_loss, ignore_index=True)
-    
-    #drop the loss_[year] lines
-    df_masked = df_masked[df_masked['code'] >= 30] 
-    
-    #create the header
+
+    # drop the loss_[year] lines
+    df_masked = df_masked[df_masked["code"] >= 30]
+
+    # create the header
     headers = [
-        {'text': 'Class', 'align': 'start', 'value': 'class'},
-        {'text': 'Area (ha)', 'value': 'area' }
+        {"text": "Class", "align": "start", "value": "class"},
+        {"text": "Area (ha)", "value": "area"},
     ]
-    
+
     items = [
-        {'class':row['class'], 'area':f'{int(row.area)}'} for index, row in df_masked.iterrows()
+        {"class": row["class"], "area": f"{int(row.area)}"}
+        for index, row in df_masked.iterrows()
     ]
-    
+
     table = v.DataTable(
-        class_             = 'ma-3',
-        headers            = headers,
-        items              = items,
-        disable_filtering  = True,
-        disable_sort       = True,
-        hide_default_footer= True
+        class_="ma-3",
+        headers=headers,
+        items=items,
+        disable_filtering=True,
+        disable_sort=True,
+        hide_default_footer=True,
     )
-    
+
     return table
-    
+
+
 def compute_ee_map(aoi_model, threshold):
-     
+
     # load the dataset and AOI
     dataset = ee.Image(cp.gfc_dataset)
     aoi = aoi_model.feature_collection
 
-    #clip the dataset on the aoi 
+    # clip the dataset on the aoi
     clip_dataset = dataset.clip(aoi)
-        
-    # create a composite band based on the user threshold 
-    calc = "gfc = (A<={0})*((C==1)*50 + (C==0)*30) + " # Non forest 
-    calc += "(A>{0})*(C==1)*(B>0)*51 + "         # gain + loss 
-    calc += "(A>{0})*(C==1)*(B==0)*50 + "        # gain 
-    calc += "(A>{0})*(C==0)*(B>0)*B + "          # loss
-    calc += "(A>{0})*(C==0)*(B==0)*40"           # stable forest
+
+    # create a composite band based on the user threshold
+    calc = "gfc = (A<={0})*((C==1)*50 + (C==0)*30) + "  # Non forest
+    calc += "(A>{0})*(C==1)*(B>0)*51 + "  # gain + loss
+    calc += "(A>{0})*(C==1)*(B==0)*50 + "  # gain
+    calc += "(A>{0})*(C==0)*(B>0)*B + "  # loss
+    calc += "(A>{0})*(C==0)*(B==0)*40"  # stable forest
 
     calc = calc.format(threshold)
-    
+
     bands = {
-        'A': clip_dataset.select('treecover2000'),
-        'B': clip_dataset.select('lossyear').unmask(0), # be carefull the 0 values are now masked
-        'C': clip_dataset.select('gain'),
+        "A": clip_dataset.select("treecover2000"),
+        "B": clip_dataset.select("lossyear").unmask(
+            0
+        ),  # be carefull the 0 values are now masked
+        "C": clip_dataset.select("gain"),
     }
-    
+
     gfc = clip_dataset.expression(calc, bands)
-    
-    return gfc.select('gfc').uint8()
+
+    return gfc.select("gfc").uint8()
+
 
 def export_legend(filename):
-    
-    #create a color list
+
+    # create a color list
     color_map = []
-    for index in cp.gfc_colors: 
+    for index in cp.gfc_colors:
         color_map.append([val for val in list(cp.gfc_colors[index])])
 
-    columns = ['entry']
-    rows = [' '*10 for index in cp.gfc_colors] #trick to see the first column
+    columns = ["entry"]
+    rows = [" " * 10 for index in cp.gfc_colors]  # trick to see the first column
     cell_text = [[index] for index in cp.gfc_colors]
 
-    fig, ax = plt.subplots(1,1, figsize=[6.4, 8.6])
+    fig, ax = plt.subplots(1, 1, figsize=[6.4, 8.6])
 
-    #remove the graph box
-    ax.axis('tight')
-    ax.axis('off')
+    # remove the graph box
+    ax.axis("tight")
+    ax.axis("off")
 
-    #set the tab title
-    ax.set_title('Raster legend')
+    # set the tab title
+    ax.set_title("Raster legend")
 
-    #create the table
+    # create the table
     the_table = ax.table(
-        colColours = [to_rgba('lightgrey')],
-        cellText   = cell_text,
-        rowLabels  = rows,
-        colWidths  = [.4],
-        rowColours = color_map,
-        colLabels  = columns,
-        loc        = 'center'
+        colColours=[to_rgba("lightgrey")],
+        cellText=cell_text,
+        rowLabels=rows,
+        colWidths=[0.4],
+        rowColours=color_map,
+        colLabels=columns,
+        loc="center",
     )
     the_table.scale(1, 1.5)
 
-    #save &close
+    # save &close
     plt.savefig(filename)
     plt.close()
-    
+
     return
--- component/scripts/utils.py
+++ component/scripts/utils.py
@@ -4,38 +4,39 @@ import pandas as pd
 
 from component import parameter as cp
 
+
 def pixel_count(raster):
-    """ produce the histogramm of each values in the raster in pixels
-  
-    Args: 
+    """produce the histogramm of each values in the raster in pixels
+
+    Args:
         raster(str): the pathname to the raster used to perform the histogramm
-    
+
     Returns:
         hist (): the histogram to be used
     """
-    
-    #readt the file 
+
+    # readt the file
     with rio.open(raster) as src:
         info = src.read(1, masked=True)
 
-    #extract the frequency of each value 
+    # extract the frequency of each value
     array = np.array(info.ravel())
     codes, frequency = np.unique(array, return_counts=True)
-    
-    columns = ['code', 'pixels']
-    hist = pd.DataFrame([[codes[i], frequency[i]] for i in range(len(codes))], columns=columns)
-    
-    #drop the 0 line (no-data)
-    hist = hist[hist['code'] != 0]
-    
-    #add the labels 
-    hist['class'] = cp.gfc_labels
-    
+
+    columns = ["code", "pixels"]
+    hist = pd.DataFrame(
+        [[codes[i], frequency[i]] for i in range(len(codes))], columns=columns
+    )
+
+    # drop the 0 line (no-data)
+    hist = hist[hist["code"] != 0]
+
+    # add the labels
+    hist["class"] = cp.gfc_labels
+
     return hist
+
 
 def to_hectar(x, resx, resy):
     """convert a pixel number into a surface in hectar using the provided resolution (res in meters)"""
-    return x*resx*resy/10000
-    
-    
-
+    return x * resx * resy / 10000
